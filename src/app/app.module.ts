6e5c05618c50c3077cdcc38eb26a9dc7f43a0b47
Kazeem Quadri <quadrikazeem01@gmail.com>
2021-06-29 20:30:32 +0100

    Adding Angular Materia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product/product.component';
+import { MaterialModule } from "./material/material.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
